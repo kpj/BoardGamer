2911cccb7c555a54b81f0f8dae5e6a9b374d5795
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2016-03-18 20:38:42 +0100

    Add simple piece movement

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
 
 let YAML = require('yamljs')
 
+function listContainsDict (list, ele) {
+  // wtf .. .includes() .. wtf
+  for (let e of list) {
+    if (e.x === ele.x && e.y === ele.y) {
+      return true
+    }
+  }
+  return false
+}
+
 class Piece {
   /*
    * Single piece on the board
@@ -28,8 +38,8 @@ class Board {
     }
   }
 
-  setPiece (pos, piece) {
-    let transPos = this.transformCoordinates(pos)
+  setPiece (viewport, pos, piece) {
+    let transPos = this.transformCoordinates(viewport, pos)
 
     if (this.board[transPos.y][transPos.x] !== undefined) {
       throw `Board position ${transPos.x}x${transPos.y} in use`
@@ -38,21 +48,26 @@ class Board {
     this.board[transPos.y][transPos.x] = piece
   }
 
-  setCurrentPlayer (player) {
-    this.player = player
+  getPiece (viewport, pos) {
+    let transPos = this.transformCoordinates(viewport, pos)
+    return this.board[transPos.y][transPos.x]
   }
 
-  transformCoordinates (pos) {
+  movePiece(viewport, startPos, endPos) {
+    let transStartPos = this.transformCoordinates(viewport, startPos)
+    let transEndPos = this.transformCoordinates(viewport, endPos)
+
+    this.board[transEndPos.y][transEndPos.x] = this.board[transStartPos.y][transStartPos.x]
+    this.board[transStartPos.y][transStartPos.x] = undefined
+  }
+
+  transformCoordinates (viewport, pos) {
     /*
      * Transform given coordinates accodring to current player
      */
-    if (this.player === undefined) {
-      throw 'No player set'
-    }
-
     return {
-      x: this.player.origin.x + this.player.origin.xf * pos.x,
-      y: this.player.origin.y + this.player.origin.yf * pos.y
+      x: viewport.origin.x + viewport.origin.xf * pos.x,
+      y: viewport.origin.y + viewport.origin.yf * pos.y
     }
   }
 }
@@ -61,9 +76,12 @@ class Game {
   /*
    * Base class of every game
    */
-  constructor (name) {
+  constructor (name, players) {
     this.name = name
+    this.players = players
+
     this.board = undefined
+    this.currentPlayerIndex = -1
   }
 
   showState () {
@@ -82,6 +100,69 @@ class Game {
       console.log()
     }
   }
+
+  startGame () {
+    this.currentPlayerIndex = 0
+  }
+
+  nextTurn () {
+    /*
+     * Cycle players
+     */
+    this.currentPlayerIndex++
+    if (this.currentPlayerIndex >= this.players.length) {
+      this.currentPlayerIndex = 0
+    }
+  }
+
+  handleInput (startPos, endPos) {
+    /*
+     * Advance game on successful move
+     */
+    try {
+      this.attemptMove(startPos, endPos)
+    } catch (err) {
+      console.log(`Invalid move (${err}), try again`);
+    }
+
+    this.nextTurn()
+  }
+
+  attemptMove (startPos, endPos) {
+    /*
+     * Attempt to move piece.
+     * Throw exception if it doesn't work
+     */
+    if (this.currentPlayerIndex === -1) {
+      throw 'Game has not started yet'
+    }
+
+    let curPlayer = this.players[this.currentPlayerIndex]
+    let piece = this.board.getPiece(curPlayer, startPos)
+
+    // check if there is a piece at all
+    if (piece === undefined) {
+      throw 'No piece selected'
+    }
+
+    // check if piece belongs to current player
+    if (piece.owner.name !== curPlayer.name) {
+      throw 'Trying to play opponent\'s piece'
+    }
+
+    let rel_move = {
+      x: (endPos.x - startPos.x) * curPlayer.origin.xf,
+      y: (endPos.y - startPos.y) * curPlayer.origin.yf
+    }
+
+    // check if move is valid
+    if (!listContainsDict(piece.spec.moves, rel_move)) {
+      throw 'Attempting invalid move'
+    }
+
+    // do move
+    this.board.movePiece(curPlayer, startPos, endPos)
+  }
 }
 
 class GameCreator {
@@ -93,7 +174,7 @@ class GameCreator {
   }
 
   generateGame () {
-    let game = new Game(this.spec.name)
+    let game = new Game(this.spec.name, this.spec.players)
 
     // setup Board
     let board = new Board(this.spec.fieldSize.width, this.spec.fieldSize.height)
@@ -101,8 +182,7 @@ class GameCreator {
     for (let player of this.spec.players) {
       for (let piece of this.spec.pieces) {
         for (let pos of piece.initialPositions) {
-          board.setCurrentPlayer(player)
-          board.setPiece(pos, new Piece(piece, player))
+          board.setPiece(player, pos, new Piece(piece, player))
         }
       }
     }
@@ -116,4 +196,10 @@ class GameCreator {
 let gc = new GameCreator('rules.yml')
 let game = gc.generateGame()
 
+game.startGame()
+
+game.showState()
+game.handleInput({x: 0, y: 1}, {x: 0, y: 2})
+game.showState()
+game.handleInput({x: 0, y: 1}, {x: 0, y: 2})
 game.showState()
